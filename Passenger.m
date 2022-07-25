22bcd4f5b57f5cc5cd1892018758ea95a64b3c5d
YUECHUYU <75524174+YUECHUYU@users.noreply.github.com>
2022-07-25 23:39:03 +0800

    Add files via upload

---
+++ Passenger.m
@@ -0,0 +1,28 @@
+//
+//  Passenger.m
+//  College Demo
+//
+//  Created by mashujun on 2022/7/18.
+//
+
+#import "Passenger.h"
+
+
+@implementation Orders
+
+@end
+
+
+@implementation Passenger
+// »•∂©∆±
+ -(void)booking:(Orders *)bookingorder{
+      [notravelorder  addObject:bookingorder];
+ }
+
+ // »•ºÏ∆±
+ -(void)checking:(Orders *)checkingorder{
+      [historyorder  addObject:checkingorder];
+      [notravelorder removeObject:checkingorder];
+ }
+
+@end
